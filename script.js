f006d1d68b3f97f7c5d1c6570b335a81ad324d71
heartylion <eliyasahamed492@gmail.com>
2024-02-09 17:10:38 +0530

    background changed due to visiblity

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function submitBooking() {
           body {
             font-family: 'Arial', sans-serif;
             margin: 20px;
-            background-image: url('ho.jpg'); 
+            background-image: url('bean.jpg'); 
             background-size: cover;
           }
           h2 {
